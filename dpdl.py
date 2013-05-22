24d87d6f8875a89419a80f47a952fdb633f76170
Koray Al <admin@koray-al.com>
2013-05-22 19:42:01 +0300

    kod temizliği

--- dpdl.py
+++ dpdl.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
-import re, sys, os, cookielib
+import re
+import sys
+import os
+import cookielib
 import mechanize
 import rarfile
 from BeautifulSoup import BeautifulSoup
+
 
 def getmediaUrl(mediaArgs):
     if len(mediaArgs) > 1:
@@ -26,25 +30,28 @@ def getmediaUrl(mediaArgs):
     br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
     # User-Agent (this is cheating, ok?)
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    br.addheaders = [('User-agent',
+                      'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) '
+                      'Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
     br.open("http://www.google.com")
     # Select the search box and search for 'foo'
-    br.select_form( 'f' )
-    br.form[ 'q' ] = query
+    br.select_form('f')
+    br.form['q'] = query
     br.submit()
     page = br.response().read()
     soup = BeautifulSoup(page)
 
     linkdictionary = []
     query.replace(" ", "-")
-    for li in soup.findAll('li', attrs={'class':'g'}):
+    for li in soup.findAll('li', attrs={'class': 'g'}):
         sLink = li.find('a')
-        sSpan = li.find('span', attrs={'class':'st'})
+        sSpan = li.find('span', attrs={'class': 'st'})
         if sLink:
-            linkurl = re.search(r"\/url\?q=(http:\/\/divxplanet.com\/sub\/m\/[0-9]{3,8}\/.*.\.html).*", sLink["href"])
+            linkurl = re.search(r"/url\?q=(http://divxplanet.com/sub/m/[0-9]{3,8}/.*.\.html).*", sLink["href"])
             if linkurl:
-                linkdictionary.append({"text": sSpan.getText().encode('ascii', 'ignore'), "name": mediaArgs[0], "url": linkurl.group(1)})
+                linkdictionary.append(
+                    {"text": sSpan.getText().encode('ascii', 'ignore'), "name": mediaArgs[0], "url": linkurl.group(1)})
     if len(linkdictionary) == 1:
         return linkdictionary[0]["url"]
     else:
@@ -56,11 +63,12 @@ def getmediaUrl(mediaArgs):
         j = raw_input("Hangi Dizi?")
         return linkdictionary[int(j)]["url"]
 
-def search_subtitles(title, season, episode): #standard input
+
+def search_subtitles(title, season, episode):  # standard input
     # Build an adequate string according to media type
     tvurl = getmediaUrl([title])
     print "looking at", tvurl
-    divpname = re.search(r"http:\/\/divxplanet.com\/sub\/m\/[0-9]{3,8}\/(.*.)\.html", tvurl).group(1)
+    divpname = re.search(r"http://divxplanet.com/sub/m/[0-9]{3,8}/(.*.)\.html", tvurl).group(1)
     season = int(season)
     episode = int(episode)
     # Browser
@@ -81,13 +89,15 @@ def search_subtitles(title, season, episode): #standard input
     br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
     # User-Agent (this is cheating, ok?)
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    br.addheaders = [('User-agent',
+                      'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) '
+                      'Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
     url = br.open(tvurl)
     html = url.read()
     soup = BeautifulSoup(html)
     subtitles_list = []
-    i = 0
+
     # /sub/s/281212/Hannibal.html
     for link in soup.findAll('a', href=re.compile("\/sub\/s\/.*.\/%s.html" % divpname)):
         addr = link.get('href')
@@ -105,22 +115,22 @@ def search_subtitles(title, season, episode): #standard input
                 else:
                     language = "Turkish"
                     lan_short = "tr"
-                subtitles_list.append({'link'    : addr,
-                                 'movie'         : title,
-                                 'description'   : "%s" % (info[1].contents[0]),
-                                 'filename'      : "%s S%02dE%02d.%s" % (title, season, episode, lan_short),
-                                 'language_flag' : "flags/%s.gif" % lan_short,
-                                 'language_name' : language,
-                                 'sync'          : False,
-                                 'rating'        : "0" })
+                subtitles_list.append({'link': addr,
+                                       'movie': title,
+                                       'description': "%s" % (info[1].contents[0]),
+                                       'filename': "%s S%02dE%02d.%s" % (title, season, episode, lan_short),
+                                       'language_flag': "flags/%s.gif" % lan_short,
+                                       'language_name': language,
+                                       'sync': False,
+                                       'rating': "0"})
     br.close()
     return subtitles_list
 
 
-def download_subtitles (subtitles_list, pos): #standard input
-    packed = True
-    dlurl = "http://divxplanet.com%s" % subtitles_list[pos][ "link" ]
-    language = subtitles_list[pos]["language_name"]
+def download_subtitles(subtitles_list, pos):  # standard input
+
+    dlurl = "http://divxplanet.com%s" % subtitles_list[pos]["link"]
+
     # Browser
     br = mechanize.Browser()
 
@@ -139,9 +149,11 @@ def download_subtitles (subtitles_list, pos): #standard input
     br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
     # User-Agent (this is cheating, ok?)
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    br.addheaders = [('User-agent',
+                      'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) '
+                      'Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-    html = br.open(dlurl).read()
+    br.open(dlurl)
     br.select_form(name="dlform")
     br.submit()
     f = open(subtitles_list[pos]["filename"] + ".rar", "wb")
@@ -155,12 +167,13 @@ def download_subtitles (subtitles_list, pos): #standard input
     os.remove(subtitles_list[pos]["filename"] + ".rar")
     br.close()
 
+
 def main(argv):
     if argv:
         if len(argv) == 3:
             season = int(argv[1])
             episode = int(argv[2])
-            if (season > 0 and episode > 0):
+            if season > 0 and episode > 0:
                 sublist = search_subtitles(argv[0], season, episode)
                 i = 0
                 for sub in sublist:
@@ -175,6 +188,6 @@ def main(argv):
         print "Kullanim: python dpdl.py 'Dizi Adi' sezon_no bolum_no"
         print "Ornek: python dpdl.py 'game of thrones' 3 3"
 
-if __name__ == "__main__":
-   main(sys.argv[1:])
 
+if __name__ == "__main__":
+    main(sys.argv[1:])
